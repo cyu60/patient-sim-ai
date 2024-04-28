e37476be2895e6180023cc9b0685c1e1c2171b46
cyu60 <chinatchinat123@gmail.com>
2024-04-27 21:06:39 -0700

    update code to make it easier to compile

--- app/(main)/cases/[casesId]/page.tsx
+++ app/(main)/cases/[casesId]/page.tsx
@@ -1,1 +1,10 @@
 // Have the individual cases with their chat context
+const Cases = () => {
+  return (
+    <div>
+      <h1> Cases</h1>
+    </div>
+  );
+};
+
+export default Cases;
--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,20 +1,17 @@
-import { getCourses } from "@/db/queries"
+import { getCourses } from "@/db/queries";
 import { List } from "./list";
+import { json } from "stream/consumers";
 
 const CoursesPage = async () => {
-    const courses = await getCourses();
-    
-    return (
-        <div className="h-full max-w-[912px] px-3 mx-auto">
-            <h1 className="text-2xl font-bold text-neutral-700">
-                Courses
-            </h1>
-            <List
-                courses={courses}
-                activeCourseId={1}
-            />
-        </div>
-    );
+  const courses = await getCourses();
+
+  return (
+    <div className="h-full max-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">Courses</h1>
+      {JSON.stringify(courses)}
+      <List courses={courses} activeCourseId={1} />
+    </div>
+  );
 };
 
 export default CoursesPage;
