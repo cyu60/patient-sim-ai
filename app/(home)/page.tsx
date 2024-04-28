310313e5053a0ac1caf21170cb4f1d4e3b5aa880
Ako-Tako <akoide18@gmail.com>
2024-04-27 23:51:08 -0700

    header added

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,13 +8,18 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
+import Image from "next/image"
 
 const HomePage = () => {
   return (
     <div className="relative bg-white">
-      <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      {/*<header className="h-20 w-full border-b-2 border-slate-200 px-4">
         <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+  <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">*/}
+      <header className="relative z-10 h-20 w-full border-b-2 border-slate-200 px-4 bg-white">
+        <div className="flex items-center justify-between h-full">{/*rmvd lg:max-w-screen-lg mx-auto*/}
           <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+            <Image src="/patientSimLogo.png" height={80} width={80} alt="Logo" />
             <h1 className="text-2xl font-extrabold text-grey-500 tracking-wide">
               PatientSimAI
             </h1>
@@ -26,9 +31,8 @@ const HomePage = () => {
             </ClerkLoading>
             <ClerkLoaded>
               <SignedIn>
-                <UserButton />
+                <UserButton/>
               </SignedIn>
-
               <SignedOut>
                 <SignInButton mode="modal" forceRedirectUrl={"/roles"}>
                   <button className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
@@ -44,11 +48,8 @@ const HomePage = () => {
       <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
         <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            {/* <img
-              className="h-11"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            /> */}
+            <Image src="/patientSimLogo.png" height={120} width={120} alt="Logo" />
+
             <div className="hidden sm:mt-32 sm:flex lg:mt-16">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 The future of medical training with realistic, AI-driven
@@ -104,7 +105,7 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-        <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+        <div className="relative z-0 lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
           <img
             className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
             src="https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
