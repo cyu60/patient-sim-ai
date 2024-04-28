c468bc2fb1f1bfb18f10e3722dc753f9cb951dd0
cyu60 <chinatchinat123@gmail.com>
2024-04-27 22:50:22 -0700

    remove company logo

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -44,11 +44,11 @@ const HomePage = () => {
       <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
         <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            <img
+            {/* <img
               className="h-11"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
-            />
+            /> */}
             <div className="hidden sm:mt-32 sm:flex lg:mt-16">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 The future of medical training with realistic, AI-driven
